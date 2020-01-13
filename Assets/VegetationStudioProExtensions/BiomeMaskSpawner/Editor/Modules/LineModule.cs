152a46c2ee8f8553f6e5c964d297138d4ca77133
Roland <10963432+Roland09@users.noreply.github.com>
2020-01-13 11:05:16 +0100

    Creating random lines

--- Assets/VegetationStudioProExtensions/BiomeMaskSpawner/Editor/Modules/LineModule.cs
+++ Assets/VegetationStudioProExtensions/BiomeMaskSpawner/Editor/Modules/LineModule.cs
@@ -2,9 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
 namespace VegetationStudioProExtensions
 {
+    /// <summary>
+    /// Create rectangular shapes and rotate them randomly.
+    /// No clipping is performed, the shapes are intended to be just some thin forest lines for details.
+    /// No biome intersection check is performed, might be more natural this way.
+    /// </summary>
     public class LineModule
     {
         private BiomeMaskSpawnerExtensionEditor biomeMaskSpawner;
@@ -34,14 +38,71 @@ namespace VegetationStudioProExtensions
         /// <param name="bounds"></param>
         private void CreateMasks(Bounds bounds)
         {
-            LineSettings lineSettings = biomeMaskSpawner.extension.lineSettings;
-            Debug.Log("TODO: create lines");
+            LineSettings lineSettings = biomeMaskSpawner.extension.lineSettings;            
             
             for( int i=0; i < lineSettings.count; i++)
             {
+
                 float width = Random.Range(lineSettings.widthMin, lineSettings.widthMax);
                 float height = Random.Range(lineSettings.heightMin, lineSettings.heightMax);
+
+                // heuristic to make it less probable that the lines are outside the bounds
+                // however they can still be outside, e. g. because of the rotation around a point
+                // for our purpose it's acceptable
+                float offsetX = width;
+                float offsetZ = height;
+
+                float x = Random.Range(bounds.min.x + offsetX, bounds.max.x - offsetX);
+                float z = Random.Range(bounds.min.z + offsetZ, bounds.max.z - offsetZ);
+
+                float angle = Random.Range(0, 360);
+
+                List<Vector3> nodes = new List<Vector3>();
+
+                nodes.Add(new Vector3(x, 0, z));
+                nodes.Add(new Vector3(x + width, 0, z));
+                nodes.Add(new Vector3(x + width, 0, z + height));
+                nodes.Add(new Vector3(x, 0, z + height));
+
+                Vector3 center = nodes[0];
+
+                for (int j = 0; j < nodes.Count; j++)
+                {
+                    Vector3 node = nodes[j];
+                    node = VectorUtils.Rotate(node, center, angle);
+                    nodes[j] = node;
+                }
+
+                Vector3 position = new Vector3(x, 0, z);
+
+                int maskId = biomeMaskSpawner.GetNextMaskId();
+                CreateBiomeMaskArea("Biome Mask " + maskId, "Mask " + maskId, position, nodes);
+
             }
+        }
+
+        /// <summary>
+        /// Create a biome mask.
+        /// The blend distance is simply calculated using the mean vector.
+        /// </summary>
+        /// <param name="gameObjectName"></param>
+        /// <param name="maskName"></param>
+        /// <param name="position"></param>
+        /// <param name="nodes"></param>
+        private void CreateBiomeMaskArea(string gameObjectName, string maskName, Vector3 position, List<Vector3> nodes)
+        {
+            float blendDistanceMin = biomeMaskSpawner.extension.biomeSettings.biomeBlendDistanceMin;
+            float blendDistanceMax = biomeMaskSpawner.extension.biomeSettings.biomeBlendDistanceMax;
+
+            float biomeBlendDistance = UnityEngine.Random.Range(blendDistanceMin, blendDistanceMax);
+
+            // mean vector is just a simple measure. please adapt if you need more accuracy
+            Vector3 meanVector = PolygonUtils.GetMeanVector(nodes);
+
+            float blendDistance = meanVector.magnitude / 2f * biomeBlendDistance;
+
+            // create the mask using the provided parameters
+            biomeMaskSpawner.CreateBiomeMaskArea(gameObjectName, maskName, position, nodes, blendDistance);
 
         }
     }
---
+++ Assets/VegetationStudioProExtensions/Common/Editor/Utils/VectorUtils.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace VegetationStudioProExtensions
+{
+    public class VectorUtils
+    {
+        /// <summary>
+        /// Rotate one point around another.
+        /// 
+        /// https://stackoverflow.com/questions/13695317/rotate-a-point-around-another-point
+        /// Credits to Fraser
+        /// 
+        /// </summary>
+        /// <param name="position">The point to rotate.</param>
+        /// <param name="center">The center point of rotation.</param>
+        /// <param name="angleInDegrees">The rotation angle in degrees.</param>
+        /// <returns>Rotated point</returns>
+        public static Vector3 Rotate(Vector3 position, Vector3 center, float angleInDegrees)
+        {
+            float angleInRadians = angleInDegrees * (Mathf.PI / 180f);
+            float cosTheta = Mathf.Cos(angleInRadians);
+            float sinTheta = Mathf.Sin(angleInRadians);
+
+            return new Vector3
+            {
+                x = cosTheta * (position.x - center.x) - sinTheta * (position.z - center.z) + center.x,
+                y = 0,
+                z = sinTheta * (position.x - center.x) + cosTheta * (position.z - center.z) + center.z
+            };
+        }
+    }
+}
